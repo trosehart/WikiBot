eb16816f8f0b5b70e889157fc88454be83850dbe
trosehart <trosehart2000@gmail.com>
2019-09-29 10:26:01 -0400

    Nearly completed, waiting for access to Twitter API

--- WikiBot.py
+++ WikiBot.py
@@ -1,31 +1,56 @@
 from bs4 import BeautifulSoup
+import re
 import urllib.request
+import urllib
 import tweepy
 
-#function to find facts
-def findFact():
-	response = urllib.request.urlopen('https://en.wikipedia.org/wiki/Main_Page')
-	html = response.read()
 	
-	factArray = []
-	
-	
-	#have to parse website for wikipedia's facts, trying to use BeautifulSoup
-	newFact = ""
-	
-	#adding next fact to array of facts
-	factArray.append(newFact)
-	
-	return factArray
-	
-	
-	
-	
-#print(html)
+url = 'https://en.wikipedia.org/wiki/Main_Page'
 
 #just testing
-for a in range(10):
-	print(a)
+#for a in range(10):
+#	print(a)
+
+response = urllib.request.urlopen(url)
+soup = BeautifulSoup(response,'html.parser')
+
+#finds what is within the <div id=mp-dyk> tag
+div_mp_dyk = soup.find("div", {"id": "mp-dyk"})
+
+#array to store strings of facts
+factArray = []
+
+#loops through the <div id=mp-dyk> tag, finding everything with <li> tags
+for tag in div_mp_dyk.find_all('li'):
+    #prints the tag content
+	#print(tag.text)
+	
+	#adds string to array if it contains "that", which shows it is a fact
+	if 'that' in tag.text:
+		factArray.append(("Did you know " + tag.text[4:])[0:139])
+	
+for fact in factArray:
+	print(fact)
+	
+
+
+#downloads image
+images = soup.find_all('img', {'src':re.compile('.jpg')})
+imurl = "h"
+for image in images: 
+	#print(image['src']+'\n')
+	if imurl == "h":
+		imurl = "https:"+image['src']
+	
+print(imurl)
+f = open('1.jpg','wb')
+f.write(urllib.request.urlopen(imurl).read())
+f.close()
+
+image = open('1.jpg','rb')
+
+
+
 """
 #have to fill this in with my info
 consumer_key = ''
@@ -40,6 +65,10 @@ api = tweepy.API(auth)
 
 #posting facts to Twitter
 for fact in factArray:  
-    api.update_with_media(fact)
+	if "pictured" in fact:
+		api.update_with_media(image, status=fact)
+	else:
+		api.update_with_media(status=fact)
+		
     time.sleep(200)
 """
