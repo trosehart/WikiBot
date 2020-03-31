36f27636e40597567e204384f8109c41ad5ef98f
Thomas Rosehart <trosehart2000@gmail.com>
2020-03-30 20:18:13 -0600

    Updated commenter

--- RunComment.bat
+++ RunComment.bat
@@ -1,1 +1,2 @@
-"C:\cygwin64\bin\python3.6" "C:/Users/trose/OneDrive/Documents/github/WikiBot/WikiComment.py" %* pause
+"C:\cygwin64\bin\python3.6" "C:/Users/trose/OneDrive/Documents/github/WikiBot/WikiComment.py" 
+%* pause
