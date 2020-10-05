9de7fb439a10d11bb3c0a3ed712847bbf6aa936a
Thomas Rosehart <trosehart2000@gmail.com>
2020-10-04 23:07:52 -0400

    Fixed image downloading, appears to consistently work now

--- WikiBot.py
+++ WikiBot.py
@@ -8,11 +8,15 @@ from random import seed
 from random import random
 
 from PrivateKeys import *
-
+#
+#
+# TODO: Make this code less garbage, functions and stuff
+#
+#
 url = 'https://en.wikipedia.org/wiki/Main_Page'
 cur_dir = os.getcwd()
 
-seed(1)
+seed()
 ran_int = int(random()*1000000)
 image_name = str(ran_int) + ".jpg"
 
@@ -27,42 +31,34 @@ factArray = []
 
 #loops through the <div id=mp-dyk> tag, finding everything with <li> tags
 for tag in div_mp_dyk.find_all('li'):
-    #prints the tag content
-    #print(tag.text)
-
     #adds string to array if it contains "that", which shows it is a fact
     if 'that' in tag.text:
         factArray.append("Did you know " + tag.text[4:])
-	
-#for fact in factArray:
-#	print(fact)
 
 
-#downloads image
-images = soup.find_all('img', {'src':re.compile('.jpg')})
-imurl = "h"
-for image in images: 
-    #to get image for specific fact, as long as it is the first with "mp-dyk" id in tag
-    if [p.get('id') for p in image.findAllPrevious(id='mp-dyk')]:
-        if imurl == "h":
-            imurl = "https:"+image['src']
-            pos = imurl.find('jpg');
-            imurl = imurl[0:pos+3]
-            imurl = imurl.replace('thumb/','')
-            #print(imurl)
-    #print(image['src']+'\n')
-    #if imurl == "h":
-    #	imurl = "https:"+image['src']
-	
-#print(imurl)
+#find URL for image
+images = soup.find('div', {'class':'dyk-img'})
+image = images.find('img')
+imurl = "https:"+image['src']
+if 'svg' not in imurl:
+    imurl = imurl.rsplit('/', 1)[0]
+    imurl = imurl.replace('thumb/','')
 
-f = open(image_name,'wb')
-f.write(urllib.request.urlopen(imurl).read())
-f.close()
+# TODO: this needs to be fixed, this try/except should just be temporary
+try:
+    if ".webm" not in imurl:
+        #downloads image
+        f = open(image_name,'wb')
+        f.write(urllib.request.urlopen(imurl).read())
+        f.close()
 
-image = open(cur_dir + '/' + image_name,'rb')
+        image = open(cur_dir + '/' + image_name,'rb')
 
+    else:
+        image = None
 
+except:
+    image = None
 
 '''
 #have to fill this in with your info
@@ -80,15 +76,13 @@ api = tweepy.API(auth)
 #posting facts to Twitter
 for fact in factArray:  
     if "pictured" in fact or "portrait shown" in fact or "depicted" in fact or "shown)" in fact:
-        #print(fact)
         try:
             api.update_with_media(media=image, status=fact, filename=image_name)
         
         except:
-            print("Image unable to be posted")
+            print("Image unable to be posted - " + imurl)
 
     else:
-        #print(fact)
         api.update_status(fact)
     time.sleep(2)
 
